30b893cd7ba824ae3426571e8bfc7c5f90318d7e
bradsk88 <7kcwKg60tdcW>
2016-08-21 19:10:53 -0600

    Initial commit.  Extension adds a banner to the top of Mint.

---
+++ main.js
@@ -0,0 +1,40 @@
+function hasClass(node, clazz) {
+    if (!node) {
+        return false;
+    }
+    if (!node.classList) {
+        return false;
+    }
+    for (var i = 0; i < node.classList.length; i++) {
+        if (node.classList[i] == clazz) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function initializeBetterMint(leftColumnNode) {
+    var badge = document.createElement("div");
+    badge.classList = ['bm-header-badge'];
+    badge.innerHTML = "BetterMint Extension is Active";
+    leftColumnNode.innerHTML = badge.outerHTML + leftColumnNode.innerHTML
+}
+
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+            var node = mutation.addedNodes[i];
+            if (!node) {
+                continue;
+            }
+            if (hasClass(node, 'OverviewPageView')) {
+                // Add top bar?
+                initializeBetterMint(node);
+            }
+            if (node.id == 'overview-left-column') {
+//                initializeBetterMint(node);
+            }
+        }
+    });
+});
+observer.observe(document, { childList: true, subtree: true });
