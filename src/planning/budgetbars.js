25c38b6f173aed0d71569a8dc33664fcca2ee971
Brad Johnson <bradsk88@gmail.com>
2017-05-07 13:49:29 -0600

    Fix width on planning page for negative balances and add hover

--- src/planning/budgetbars.js
+++ src/planning/budgetbars.js
@@ -17,6 +17,10 @@ function PlanningBudgetBars() {
             return;
         }
         node = node.getElementsByClassName('status')[0];
+	var title = node.title;
+	node.removeAttribute('title');
+	node.getElementsByClassName('progress_bar')[0].title = title;
+
         var newBar = document.createElement('span');
         newBar.id = PREFIX + _id;
         newBar.classList = 'progress_bar bettermint_planning_supplement_bar';
@@ -26,15 +30,22 @@ function PlanningBudgetBars() {
         if (left > 511) {
             return;
         }
-        newBar.style.left = left + 'px';
-        var width = (maxWidth * getTodayAsFractionOfMonth()) - left;
+	var styleLeft = left;
+	if (left <= 0) {
+	    styleLeft = 0;
+	    newBar.classList = newBar.classList + ' left';
+	}
+        newBar.style.left = styleLeft + 'px';
+        var width = (maxWidth * getTodayAsFractionOfMonth()) - styleLeft;
         if (width < 0) { // Overspent
             return;
         }
         newBar.style.width = width + 'px';
         var remainder = (getTodayAsFractionOfMonth() * allotted) - spent;
-        newBar.innerText = '$' + (Math.floor(remainder * 100) / 100);
-        node.append(newBar);
+	var remCash = '$' + Number(Math.floor(remainder * 100) / 100).toFixed(2);
+        newBar.title = "You could spend " + remCash + " and still be on track.";
+        newBar.innerText = remCash;
+	node.append(newBar);
     };
     self.supplementAdded = function(nodeId) {
         var supplementNode = document.getElementById(PREFIX + nodeId);
