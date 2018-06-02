a0594d44325c682297f282917070afffdd2630f6
Brad Johnson <bradsk88@gmail.com>
2018-06-02 12:25:07 -0600

    WIP: Button for "clamping down" budget.

--- src/planning/budgetbars.js
+++ src/planning/budgetbars.js
@@ -8,27 +8,14 @@ function PlanningBudgetBars() {
             self.nodes.push(node);
         }
     };
-    self.doAddBudgetBarSupplement = function (node) {
-        let _id = node.id;
-        let text = node.getElementsByClassName('progress')[0].innerText;
-        let spent = getDollarsIntFromText(text.split(' of ')[0]);
-        let allotted = getDollarsIntFromText(text.split(' of ')[1]);
-        if (allotted === 0) {
-            return;
-        }
-        node = node.getElementsByClassName('status')[0];
-        let title = node.title;
-        node.removeAttribute('title');
-        node.getElementsByClassName('progress_bar')[0].title = title;
-
-        let newBar = document.createElement('span');
-        newBar.id = PREFIX + _id;
+    self.buildBudgetBar = function (spent, allotted) {
+        const newBar = document.createElement('span');
         newBar.classList = 'progress_bar bettermint_planning_supplement_bar';
         let maxWidth = 512;
         let fraction = spent / allotted;
         let left = (maxWidth * fraction);
         if (left > 511) {
-            return;
+            return null;
         }
         let styleLeft = left;
         if (left <= 0) {
@@ -38,14 +25,54 @@ function PlanningBudgetBars() {
         newBar.style.left = styleLeft + 'px';
         let width = (maxWidth * getTodayAsFractionOfMonth()) - styleLeft;
         if (width < 0) { // Overspent
-            return;
+            return null;
         }
         newBar.style.width = width + 'px';
         let remainder = (getTodayAsFractionOfMonth() * allotted) - spent;
         let remCash = '$' + Number(Math.floor(remainder * 100) / 100).toFixed(2);
         newBar.title = "You could spend " + remCash + " and still be on track.";
         newBar.innerText = remCash;
-        node.append(newBar);
+        return newBar;
+    };
+    self.clampButtonClicked = function() {
+        const updater = document.getElementById('pop-budget');
+        window['$MC']['Planning'].openBudgetPopup(); // FIXME: Don't have access to this because of sandbox
+        document.getElementById('pop-budget-2-amount').value = 111;
+        document.getElementById('pop-budget-frequency-0').value = 0;
+        // document.getElementById('pop-budget-submit').click();
+    };
+    self.buildBudgetButton = function() {
+        const newButton = document.createElement('button');
+        newButton.innerText = 'GO!';
+        newButton.classList.add('clampButton');
+        newButton.onclick = self.clampButtonClicked;
+        return newButton;
+    };
+    self.doAddBudgetBarSupplement = function (node) {
+        const parent = node;
+        const text = node.getElementsByClassName('progress')[0].innerText;
+        const spent = getDollarsIntFromText(text.split(' of ')[0]);
+        const allotted = getDollarsIntFromText(text.split(' of ')[1]);
+        if (allotted === 0) {
+            return;
+        }
+        node = node.getElementsByClassName('status')[0];
+        let title = node.title;
+        node.removeAttribute('title');
+        node.getElementsByClassName('progress_bar')[0].title = title;
+
+        const newBar = self.buildBudgetBar(spent, allotted);
+        if (newBar !== null) {
+            node.append(newBar);
+        }
+        const newButton = self.buildBudgetButton();
+        if (newButton !== null) {
+            parent.append(newButton);
+        }
+        const proof = document.createElement('span');
+        proof.id = PREFIX + parent.id;
+        parent.append(proof);
+
     };
     self.supplementAdded = function (nodeId) {
         let supplementNode = document.getElementById(PREFIX + nodeId);
