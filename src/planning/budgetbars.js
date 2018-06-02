8022acde6d0e2dc8424068d0caa7ead0f73906fa
Brad Johnson <bradsk88@gmail.com>
2018-06-02 10:50:57 -0600

    Reformat

--- src/planning/budgetbars.js
+++ src/planning/budgetbars.js
@@ -1,58 +1,58 @@
 const PREFIX = 'planning-supplement-';
 
 function PlanningBudgetBars() {
-    var self = this;
+    let self = this;
     self.nodes = [];
-    self.consider = function(node) {
+    self.consider = function (node) {
         if (!!node.parentElement && node.parentElement.id == 'spendingBudget-list-body') {
             self.nodes.push(node);
         }
     };
-    self.doAddBudgetBarSupplement = function(node) {
-        var _id = node.id;
-        var text = node.getElementsByClassName('progress')[0].innerText;
-        var spent = getDollarsIntFromText(text.split(' of ')[0]);
-        var allotted = getDollarsIntFromText(text.split(' of ')[1]);
+    self.doAddBudgetBarSupplement = function (node) {
+        let _id = node.id;
+        let text = node.getElementsByClassName('progress')[0].innerText;
+        let spent = getDollarsIntFromText(text.split(' of ')[0]);
+        let allotted = getDollarsIntFromText(text.split(' of ')[1]);
         if (allotted === 0) {
             return;
         }
         node = node.getElementsByClassName('status')[0];
-	var title = node.title;
-	node.removeAttribute('title');
-	node.getElementsByClassName('progress_bar')[0].title = title;
+        let title = node.title;
+        node.removeAttribute('title');
+        node.getElementsByClassName('progress_bar')[0].title = title;
 
-        var newBar = document.createElement('span');
+        let newBar = document.createElement('span');
         newBar.id = PREFIX + _id;
         newBar.classList = 'progress_bar bettermint_planning_supplement_bar';
-        var maxWidth = 512;
-        var fraction = spent/allotted;
-        var left = (maxWidth * fraction);
+        let maxWidth = 512;
+        let fraction = spent / allotted;
+        let left = (maxWidth * fraction);
         if (left > 511) {
             return;
         }
-	var styleLeft = left;
-	if (left <= 0) {
-	    styleLeft = 0;
-	    newBar.classList = newBar.classList + ' left';
-	}
+        let styleLeft = left;
+        if (left <= 0) {
+            styleLeft = 0;
+            newBar.classList = newBar.classList + ' left';
+        }
         newBar.style.left = styleLeft + 'px';
-        var width = (maxWidth * getTodayAsFractionOfMonth()) - styleLeft;
+        let width = (maxWidth * getTodayAsFractionOfMonth()) - styleLeft;
         if (width < 0) { // Overspent
             return;
         }
         newBar.style.width = width + 'px';
-        var remainder = (getTodayAsFractionOfMonth() * allotted) - spent;
-	var remCash = '$' + Number(Math.floor(remainder * 100) / 100).toFixed(2);
+        let remainder = (getTodayAsFractionOfMonth() * allotted) - spent;
+        let remCash = '$' + Number(Math.floor(remainder * 100) / 100).toFixed(2);
         newBar.title = "You could spend " + remCash + " and still be on track.";
         newBar.innerText = remCash;
-	node.append(newBar);
+        node.append(newBar);
     };
-    self.supplementAdded = function(nodeId) {
-        var supplementNode = document.getElementById(PREFIX + nodeId);
+    self.supplementAdded = function (nodeId) {
+        let supplementNode = document.getElementById(PREFIX + nodeId);
         return Boolean(supplementNode);
     };
-    self.batchAddComponents = function() {
-        for (var i = 0; i < self.nodes.length; i++) {
+    self.batchAddComponents = function () {
+        for (let i = 0; i < self.nodes.length; i++) {
             var node = self.nodes[i];
             setTimeout(function () {
                 if (!self.supplementAdded(node.id)) {
