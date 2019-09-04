dae0b24987a90889afdb5eb90eeb480f6d19dc96
Brad Johnson <bradsk88@gmail.com>
2019-09-03 21:38:51 -0600

    Added option page where tip percent can be set
    Using setting to enable/set tip percent in split dialog.

---
+++ src/options.js
@@ -0,0 +1,35 @@
+function toggleTip(checked) {
+    document.getElementById('tip-percent').hidden = !checked;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.sync.get('tipPercent', (v) => {
+        if (v['tipPercent'] && v['tipPercent'] !== "") {
+            document.getElementById('tip-percent-input').value = v['tipPercent'];
+            document.getElementById('tip-enable-checkbox').checked = true;
+            document.getElementById('tip-percent').hidden = false;
+        } else {
+            document.getElementById('tip-percent-input').value = 20;
+            document.getElementById('tip-enable-checkbox').checked = false;
+        }
+    });
+    document.getElementById('tip-enable-checkbox')
+        .addEventListener('change', (event) => {
+            toggleTip(event.target.checked);
+        })
+    document.getElementById('save').addEventListener('click', () => {
+        let tipPercent = "";
+        if (document.getElementById('tip-enable-checkbox').checked) {
+            tipPercent = document.getElementById('tip-percent-input').value;
+        }
+        const settings = {
+            'tipPercent': tipPercent,
+        };
+        console.log('saving', settings);
+        chrome.storage.sync.set(settings);
+        document.getElementById('saved').style.display = 'block';
+        setTimeout(() => {
+            document.getElementById('saved').style.display = 'none';
+        }, 3000);
+    });
+})
